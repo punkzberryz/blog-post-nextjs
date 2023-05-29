bc9a5bf7fcdc3e1ccc1e4fca03f4748fa5871d06
punkzberryz <16286841+punkzberryz@users.noreply.github.com>
2023-05-30 07:47:35 +0800

    post query on-going

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
-import { db, posts, comments, users } from "@/db";
+import { db, posts, comments, users, Post } from "@/db";
 import { eq } from "drizzle-orm";
+import { alias } from "drizzle-orm/mysql-core";
 
 export async function GET(
   req: NextRequest,
@@ -12,16 +13,31 @@ export async function GET(
     return new NextResponse("No post id found...", { status: 400 });
   }
 
+  const commentOwner = alias(users, "CommentOwner");
+
   const postQuery = await db
     .select()
     .from(posts)
     .where(eq(posts.id, id))
-    .rightJoin(comments, eq(comments.postId, posts.id))
-    .rightJoin(users, eq(comments.authorId, users.id));
-
+    .innerJoin(users, eq(users.id, posts.authorId))
+    .leftJoin(comments, eq(comments.postId, posts.id))
+    .innerJoin(commentOwner, eq(commentOwner.id, comments.authorId));
   if (!postQuery.length) {
     return new NextResponse("Post by Id not found...", { status: 400 });
   }
+
+  const result = postQuery.reduce<Record<number, {}>>((acc, row) => {
+    const user = row.User;
+    const post = row.Post;
+    const comment = row.Comment;
+    const commentOwner = row.CommentOwner;
+    if (!acc[post.id]) {
+      acc[post.id] = { post, comments: [] };
+    }
+    console.log(row);
+    return {};
+  }, {});
+
   const post = postQuery;
   const response = NextResponse.json({ post });
 
